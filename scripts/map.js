f95c2a41372b6814daef90781948c86fcbdf3af5
goodneighboriowa <78562904+goodneighboriowa@users.noreply.github.com>
2021-04-07 11:17:07 -0500

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -153,9 +153,9 @@ $(window).on('load', function() {
       if (clusters) {
         // Add multilayer cluster support
         multilayerClusterSupport = L.markerClusterGroup.layerSupport({
-          maxClusterRadius:2000,
-          disableClusteringAtZoom:1,
-          );
+            maxClusterRadius: 10,
+            disableClusteringAtZoom: 10,
+        });
         multilayerClusterSupport.addTo(map);
 
         for (i in layers) {
